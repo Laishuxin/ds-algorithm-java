3549ae93ffcdd3ece5c720f57623499820119492
Laishuxin <15622971239@163.com>
2021-08-26 20:27:22 +0800

    feat(linear): linear search

---
+++ src/LinearAlgorithm/LinearSearch/LinearSearch.java
@@ -0,0 +1,19 @@
+package LinearAlgorithm.LinearSearch;
+
+/**
+ * Project  algorithm
+ *
+ * @author LaiShuXin
+ * @time 2021-08-26 0:40
+ */
+public class LinearSearch {
+
+    private LinearSearch() {}
+
+    public static <T> int linearSearch(T[] arr, T target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(target)) return i;
+        }
+        return -1;
+    }
+}
---
+++ src/LinearAlgorithm/LinearSearch/LinearSearchTest.java
@@ -0,0 +1,36 @@
+package LinearAlgorithm.LinearSearch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Project  algorithm
+ *
+ * @author LaiShuXin
+ * @time 2021-08-26 8:57
+ */
+class LinearSearchTest {
+
+    static Integer[] arrayGenerator(int length) {
+        Integer[] arr = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void linearSearch() {
+        Integer[] arr = arrayGenerator(10);
+        assertEquals(LinearSearch.linearSearch(arr, 100), -1);
+        assertEquals(LinearSearch.linearSearch(arr, 1), 1);
+        assertEquals(LinearSearch.linearSearch(arr, 0), 0);
+        assertEquals(LinearSearch.linearSearch(arr, 9), 9);
+        assertEquals(LinearSearch.linearSearch(arr, 5), 5);
+    }
+}
